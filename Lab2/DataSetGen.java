329439924e424840fbc8b7ffcb67887aa3306c0f
02ctark <89858227+02ctark@users.noreply.github.com>
2022-02-07 13:26:25 -0500

    Add files via upload

---
+++ Lab2/DataSetGen.java
@@ -0,0 +1,74 @@
+
+public class DataSetGen<T extends Measurable>{
+
+	
+	private double sum;
+	private T maximum;
+	private int count;
+
+	
+	/**
+	 *  Assigning all the constructors to 0
+	 */
+	public DataSetGen() {
+		
+		sum = 0;
+		maximum = null;
+		count = 0;
+		
+		
+	}
+	
+	
+	
+	/**
+	 * retrieve the average of that data type
+	 * 
+	 * @return sum/count
+	 */
+
+	public double getAverage() {
+		// TODO Auto-generated method stub
+		
+		
+		if (count ==0) {
+			return 0;
+		}else {
+			
+			return (sum/count);
+		}
+	
+	}
+	
+	/**
+	 * retrieve max value
+	 * 
+	 * @return maximum
+	 */
+
+	public T getMaximum() {
+		// TODO Auto-generated method stub\
+		
+		return maximum;
+	}
+
+	
+	/**
+	 * adds data value to data set
+	 * 
+	 * @param m
+	 */
+	public void add(T t) {
+		// TODO Auto-generated method stub
+		
+		sum += t.getMeasure();
+		
+		if(count == 0 || maximum.getMeasure() < t.getMeasure()) {
+			
+			count++;
+			
+		}
+		
+	}
+
+}
